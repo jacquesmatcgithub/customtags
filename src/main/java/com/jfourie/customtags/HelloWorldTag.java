77a70ced9da98a51937d1a6cca0e77183338ab3e
Jacques Fourie <jfourie@madisoncollege.edu>
2018-01-31 21:00:39 -0600

    Working on week3 exercise.

--- src/main/java/com/jfourie/customtags/HelloWorldTag.java
+++ src/main/java/com/jfourie/customtags/HelloWorldTag.java
@@ -4,6 +4,9 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class HelloWorldTag extends SimpleTagSupport {
     @Override
@@ -11,6 +14,45 @@ public class HelloWorldTag extends SimpleTagSupport {
         super.doTag();
 
         JspWriter out = getJspContext().getOut();
-        out.println("Hello Enterprise Java Class");
+
+        String salutation = "Good ";
+        int hourNow = LocalDateTime.now().getHour();
+
+        if (hourNow >= 1 && hourNow < 12) {
+            salutation = salutation + "morning ";
+        } else if (hourNow >= 12 && hourNow < 17) {
+            salutation = salutation + "afternoon ";
+        } else {
+            salutation = salutation + "evening ";
+        }
+
+        salutation = salutation + "enterprise java class.";
+
+        out.println(salutation + "<br/>");
+
+
+        LocalDate localDate = LocalDate.now();
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        salutation = "Today is " + dayOfWeek + ".";
+        out.println(salutation + "<br/>");
+
+        int month = localDate.getMonthValue();
+        int day = localDate.getDayOfMonth();
+
+        if (month == 12 && day == 24) {
+            salutation = "Tomorrow is Christmas Day.";
+        } else if (month == 12 && day == 30) {
+            salutation = "Tomorrow is new years eve.";
+        } else if (month == 12 && day == 31) {
+            salutation = "Tomorrow is new years day.";
+        } else if (month == 2 && day == 13) {
+            salutation = "Tomorrow is Valentines Day.";
+        } else {
+            salutation = "Tomorrow is just another day.";
+        }
+
+        out.println(salutation + "<br/>");
+
     }
 }
